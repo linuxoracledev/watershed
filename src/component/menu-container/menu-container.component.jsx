0ea6382bad200a2c6893c496088ce0e84c8884b1
Azad <linuxoracledev@gmail.com>
2022-01-01 12:02:21 +0600

    Breakdown home page into component and style menu item

---
+++ src/component/menu-container/menu-container.component.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import MenuItem from '../menu-item/menu-item.component';
+import '../menu-container/menu-container.styles.scss';
+
+class MenuContainer extends React.Component {
+
+    constructor() {
+        super();
+
+        this.state = {
+            menuSection: [
+                {
+                    title: 'hats',
+                    imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+                    id: 1,
+                    linkUrl: 'shop/hats'
+                },
+                {
+                    title: 'jackets',
+                    imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+                    id: 2,
+                    linkUrl: 'shop/jackets'
+                },
+                {
+                    title: 'sneakers',
+                    imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+                    id: 3,
+                    linkUrl: 'shop/sneakers'
+                },
+                {
+                    title: 'womens',
+                    imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+                    size: 'large',
+                    id: 4,
+                    linkUrl: 'shop/womens'
+                },
+                {
+                    title: 'mens',
+                    imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+                    size: 'large',
+                    id: 5,
+                    linkUrl: 'shop/mens'
+                }
+            ]
+        }
+    }
+
+    render() {
+        return (
+            <div className="menu-continer">
+                {this.state.menuSection.map(sec => (
+                    <MenuItem key={sec.id} title={sec.title} imgurl ={sec.imageUrl}  size ={sec.size}/>
+                ))}
+
+            </div>
+        )
+    }
+
+
+}
+
+export default MenuContainer;
---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './homepage.styles.scss';
+import MenuContainer from "../../component/menu-container/menu-container.component";
+
+const HomePage = () => (
+
+    <div className="homepage">
+        <MenuContainer />
+    </div>
+
+);
+export default HomePage;

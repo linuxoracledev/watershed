916a3e26c2d4b8cb99f784c616de163fc81b52ca
Azad <linuxoracledev@gmail.com>
2022-01-08 10:56:40 +0600

    Add Header and link to Home page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import CategoryPage from './pages/category/category.component';
+import Header from './component/header/header.component';
 import { Routes, Route, BrowserRouter as BRuter } from 'react-router-dom';
 
 function App() {
   return (
-    /*<HomePage />*/
-
     <BRuter>
+      <Header />
       <Routes>
         <Route exact path='/' element={<HomePage />} />
         <Route path='/category' element={<CategoryPage />} />
